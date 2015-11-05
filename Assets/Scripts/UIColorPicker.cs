f835b6c294178638a979c8353f3be748066d5de2
TheDarkVoid <Rohit.Khamraj@gmail.com>
2015-11-05 00:16:08 -0500

    62.2

--- Assets/Scripts/UIColorPicker.cs
+++ Assets/Scripts/UIColorPicker.cs
@@ -9,14 +9,11 @@ public class UIColorPicker : MonoBehaviour
 	public Slider slider1;
 	public Slider slider2;
 	public Slider slider3;
+	public float s1, s2, s3;
 
 	//Private
 	private int _mode;
 
-	void Start()
-	{
-		
-	}
-
 	
+
 }
